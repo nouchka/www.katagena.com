6e5f474963185b92ddb2b744cdcc12308c866bc9
nouchka <github@katagena.com>
2018-06-10 16:19:20 +0000

    no more google

--- web/index.php
+++ web/index.php
@@ -22,15 +22,6 @@ a {
 </style>
 </head>
 <body>
-<?php
-require '../vendor/autoload.php';
-use AntonioTajuelo\Gtm\Gtm;
-
-Gtm::renderContainer ( 'GTM-T9CJCZK' );
-Gtm::datalayerPush ( [ 
-		'website' => 'home' 
-] );
-?>
 	<!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
