8ffe2ffc2fd6a6eaff4cd5cbbabbf318e6206df7
nouchka <dev@zen-spirit.net>
2019-05-10 22:26:42 +0200

    test

--- web/faas/semver.php
+++ web/faas/semver.php
@@ -2,10 +2,9 @@
 
 //TEST
 
-$now = time( );
-$then = gmstrftime("%a, %d %b %Y %H:%M:%S GMT", $now + 365*86440);
 $etag = md5($_GET['version']);
-header("Expires: $then");
 header("Etag: $etag");
+header('Cache-Control: max-age=864000');
 
 echo "4.2.0";
+echo $etag;
