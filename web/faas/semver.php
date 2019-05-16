eaafcde5fb9e235c3a5b3c103c2250f96a93f694
nouchka <dev@zen-spirit.net>
2019-05-16 21:09:30 +0000

    test

--- web/faas/semver.php
+++ web/faas/semver.php
@@ -4,9 +4,16 @@ include '../../vendor/autoload.php';
 use Rollerworks\Component\Version\Version;
 use Rollerworks\Component\Version\VersionsValidator;
 
+$sentryUrl = getenv("SENTRY_URL");
+if (isset($sentryUrl)) {
+    Sentry\init([
+        'dsn' => $sentryUrl
+    ]);
+}
+
 $version = $_GET['version'];
-$now = time( );
-$then = gmstrftime("%a, %d %b %Y %H:%M:%S GMT", $now + 365*86440);
+$now = time();
+$then = gmstrftime("%a, %d %b %Y %H:%M:%S GMT", $now + 365 * 86440);
 header("Expires: $then");
 $etag = md5($version);
 header("Etag: $etag");
@@ -14,7 +21,7 @@ header('Cache-Control: public, max-age=864000');
 
 if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag) {
     header("HTTP/1.1 304 Not Modified");
-    exit;
+    exit();
 }
 
 $version = Version::fromString($version);
