2f8dc2a83ae0b84dc78b606cc8bfae7d8dce7f64
nouchka <dev@zen-spirit.net>
2019-06-24 20:59:03 +0000

    manage errors

--- web/faas/semver.php
+++ web/faas/semver.php
@@ -1,6 +1,7 @@
 <?php
 include '../../vendor/autoload.php';
 
+// #https://github.com/rollerworks/version
 use Rollerworks\Component\Version\Version;
 use Rollerworks\Component\Version\VersionsValidator;
 
@@ -11,7 +12,27 @@ if (isset($sentryUrl)) {
     ]);
 }
 
+$up = $_GET['up'];
+if (! in_array($up, [
+    'major',
+    'minor',
+    'patch',
+    'next',
+    'patch',
+    'stable',
+    'alpha',
+    'beta',
+    'rc'
+])) {
+    echo 'wrong up parameter';
+    exit();
+}
 $version = $_GET['version'];
+
+if (! preg_match('/^v?' . Rollerworks\Component\Version\Version::VERSION_REGEX . '$/i', $version, $matches)) {
+    echo 'wrong version';
+    exit();
+}
 $now = time();
 $then = gmstrftime("%a, %d %b %Y %H:%M:%S GMT", $now + 365 * 86440);
 header("Expires: $then");
@@ -26,7 +47,7 @@ if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && trim($_SERVER['HTTP_IF_NONE_MATCH']
 
 $version = Version::fromString($version);
 
-$newVersion = $version->getNextIncreaseOf('major');
+$newVersion = $version->getNextIncreaseOf($up);
 
 echo $newVersion;
 
